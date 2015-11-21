2be02dc5fa04106d9999fd4edf61c469cf9680f4
lixander <lixandercad@gmail.com>
2015-11-21 11:10:45 -0500

    Running

--- test/empresariales-ic-node_test.js
+++ test/empresariales-ic-node_test.js
@@ -6,6 +6,7 @@ var assert = require('should');
 describe('empresarialesIcNode', function () {
 
   it('should be awesome', function () {
+  	
     empresarialesIcNode().should.equal('awesome');
   });
 
